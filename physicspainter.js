d4266c839056da0f5febff3d924791067c55aa86
Will McCutchen <will@mccutch.org>
2020-11-25 21:08:03 -0500

    more renderer

--- physicspainter.js
+++ physicspainter.js
@@ -12,6 +12,19 @@ const RunSketch = (function () {
 
     const CHASER_REPULSION = RENDER_MIN_LINE_WIDTH;
 
+    // https://coolors.co/ffe0e9-ffc2d4-ff9ebb-ff7aa2-e05780-b9375e-8a2846-602437-522e38
+    const COLORS = [
+        "#fadde1",
+        "#ffc4d6",
+        "#ffa6c1",
+        "#ff87ab",
+        "#ff5d8f",
+        "#ff97b7",
+        "#ffacc5",
+        "#ffcad4",
+        "#f4acb7",
+    ];
+
     // Entrypoint
     function RunSketch(canvas) {
         const ctx = canvas.getContext("2d");
@@ -25,9 +38,9 @@ const RunSketch = (function () {
         targets.push(target);
 
         const chasers = [];
-        chasers.push(new Chaser('#f372cc', target, w, h, 150, 1.1, CHASER_REPULSION));
-        chasers.push(new Chaser('#f8961e', target, w, h, 170, 1.08, CHASER_REPULSION));
-        chasers.push(new Chaser('#f9c74f', target, w, h, 140, 1.12, CHASER_REPULSION));
+        chasers.push(new Chaser(target, w, h, 150, 1.1, CHASER_REPULSION));
+        chasers.push(new Chaser(target, w, h, 170, 1.08, CHASER_REPULSION));
+        chasers.push(new Chaser(target, w, h, 140, 1.12, CHASER_REPULSION));
 
         const renderer = new Renderer(ctx, w, h, targets, chasers);
 
@@ -95,26 +108,13 @@ const RunSketch = (function () {
                 this.dy *= -1;
             }
         }
-        wrap() {
-            if (this.x < 0) { this.x += this.w; }
-            if (this.x > this.w) { this.x %= this.w; }
-            if (this.y < 0) { this.y += this.h; }
-            if (this.y > this.h) { this.y %= this.h; }
-        }
-        teleport() {
-            if (this.x < 0 || this.x > this.w || this.y < 0 || this.y > this.h) {
-                this.x = rand(0, this.w);
-                this.y = rand(0, this.h);
-            }
-        }
     }
 
     // Chaser models a point that always moves towards a given target with its
     // movement governed by a crude physics implementation based on friction,
     // elasticity, and repulsion parameters.
     class Chaser {
-        constructor(color, target, w, h, f, e, r) {
-            this.color = color;
+        constructor(target, w, h, f, e, r) {
             this.target = target;
             this.f = f; // friction
             this.e = e; // elasticity
@@ -136,7 +136,7 @@ const RunSketch = (function () {
             this.vy = (this.vy + (dy / this.f)) / this.e;
 
             // steer away from other chasers
-            for (var other of others) {
+            for (let other of others) {
                 if (this === other) {
                     continue;
                 }
@@ -157,12 +157,17 @@ const RunSketch = (function () {
     }
 
     class Renderer {
-        constructor(ctx, w, h, targets, chasers) {
+        constructor(ctx, w, h, targets, chasers, palette) {
             this.ctx = ctx;
             this.w = w;
             this.h = h;
             this.targets = targets;
             this.chasers = chasers;
+
+            // Pick a color for each chaser
+            this.palette = this.chasers.map((chaser, i) => {
+                return COLORS[Math.floor(Math.random() * COLORS.length)];
+            });
 
             // track last position of each chaser
             this.lastx = this.chasers.map(chaser => chaser.x);
@@ -178,12 +183,13 @@ const RunSketch = (function () {
             ctx.lineJoin = 'round';
             for (let i = 0; i < chasers.length; i++) {
                 let chaser = chasers[i];
+                let color = this.palette[i];
                 let lastx = this.lastx[i];
                 let lasty = this.lasty[i];
 
                 let lineWidth = Math.max(RENDER_MIN_LINE_WIDTH, Math.min((Math.abs(chaser.vx) + Math.abs(chaser.vy) * RENDER_LINE_WIDTH_FACTOR), RENDER_MAX_LINE_WIDTH)) | 0;
                 ctx.lineWidth = lineWidth;
-                ctx.strokeStyle = chaser.color;
+                ctx.strokeStyle = color;
 
                 let x0 = lastx - lineWidth;
                 let y0 = lasty - lineWidth;
