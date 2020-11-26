2b10e27c4a6f22caeee1cf0b8d4a90eeac70d914
Will McCutchen <will@mccutch.org>
2020-11-25 19:56:04 -0500

    a bit better

--- physicspainter.js
+++ physicspainter.js
@@ -1,36 +1,91 @@
 const RunSketch = (function () {
     'use strict';
 
+    const TARGET_STEP_RANGE = 50;
+    const TARGET_STEP_PERIOD = 20;
+    const TARGET_STEP_PROBABILITY = 0.75;
+
+    const RENDER_MIN_LINE_WIDTH = 6;
+    const RENDER_MAX_LINE_WIDTH = 50;
+    const RENDER_LINE_WIDTH_FACTOR = 10;
+
+    const CHASER_REPULSION = RENDER_MIN_LINE_WIDTH;
+
+    // Entrypoint
+    function RunSketch(canvas) {
+        const ctx = canvas.getContext("2d");
+        setupCanvas(canvas);
+
+        const w = canvas.width;
+        const h = canvas.height;
+
+        const targets = [];
+        let target = new Target(w, h, TARGET_STEP_RANGE, TARGET_STEP_PROBABILITY);
+        targets.push(target);
+
+        const chasers = [];
+        chasers.push(new Chaser('#f372cc', target, w, h, 150, 1.1, CHASER_REPULSION));
+        chasers.push(new Chaser('#f8961e', target, w, h, 170, 1.08, CHASER_REPULSION));
+        chasers.push(new Chaser('#f9c74f', target, w, h, 140, 1.12, CHASER_REPULSION));
+
+        (function loop(t) {
+            window.requestAnimationFrame(function frame() {
+                if (t % TARGET_STEP_PERIOD === 0) {
+                    for (var target of targets) {
+                        target.step();
+                    }
+                }
+                for (var chaser of chasers) {
+                    chaser.step(chasers);
+                }
+                render(ctx, w, h, targets, chasers);
+                loop(t + 1);
+            });
+        })(0);
+    }
+
     class Target {
-        constructor(w, h, range) {
+        constructor(w, h, range, stepProbability) {
             this.w = w;
             this.h = h;
             this.range = range;
+            this.stepProbability = stepProbability;
 
-            this.x = rand(0, w);
-            this.y = rand(0, h);
+            this.x = (w / 2) + rand(-this.range, this.range);
+            this.y = (h / 2) + rand(-this.range, this.range);
             this.dx = 0;
             this.dy = 0;
             this.step();
         }
         step() {
-            if (Math.random() < 0.5) {
+            if (Math.random() < this.stepProbability) {
                 this.dx = rand(-this.range, this.range);
                 this.dy = rand(-this.range, this.range);
             }
-
             this.x += this.dx;
             this.y += this.dy;
+            this.constrain();
         }
         constrain() {
-            // this.wrap();
             this.bounce();
         }
         bounce() {
-            if (this.x < 0) { this.x *= -1; }
-            if (this.x > this.w) { this.x -= this.w % this.x; }
-            if (this.y < 0) { this.y *= -1; }
-            if (this.y > this.h) { this.y -= this.h % this.y; }
+            if (this.x < 0) {
+                this.x *= -1;
+                this.dx *= -1;
+            }
+            if (this.x > this.w) {
+                this.x -= this.x % this.w;
+                this.dx *= -1;
+            }
+            if (this.y < 0) {
+                this.y *= -1;
+                this.dy *= -1;
+            }
+            if (this.y > this.h) {
+                this.y -= this.y % this.h;
+                this.dy *= -1;
+            }
         }
         wrap() {
             if (this.x < 0) { this.x += this.w; }
@@ -48,8 +103,8 @@ const RunSketch = (function () {
             this.e = e; // elasticity
             this.r = r; // repulsion
 
-            this.x = (w / 2) + rand(-20, 20);
-            this.y = (h / 2) + rand(-20, 20);
+            this.x = (w / 2);
+            this.y = (h / 2);
             this.lastx = this.x;
             this.lasty = this.y;
             this.vx = 0;
@@ -89,8 +144,10 @@ const RunSketch = (function () {
     }
 
     function render(ctx, w, h, targets, chasers) {
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
         for (var chaser of chasers) {
-            let lineWidth = Math.min((Math.abs(chaser.vx) + Math.abs(chaser.vy) * 0.75 | 0), 30);
+            let lineWidth = Math.max(RENDER_MIN_LINE_WIDTH, Math.min((Math.abs(chaser.vx) + Math.abs(chaser.vy) * RENDER_LINE_WIDTH_FACTOR), RENDER_MAX_LINE_WIDTH)) | 0;
             ctx.lineWidth = lineWidth;
             ctx.strokeStyle = chaser.color;
 
@@ -139,48 +196,5 @@ const RunSketch = (function () {
         return (Math.random() * (max - min) + min) | 0;
     }
 
-    return function RunSketch(canvas) {
-        const ctx = canvas.getContext("2d");
-        setupCanvas(canvas);
-
-        const w = canvas.width;
-        const h = canvas.height;
-
-        const targets = [];
-        let target = new Target(w, h, 100);
-        targets.push(target);
-
-        const chasers = [];
-        chasers.push(new Chaser('#f372cc', target, w, h, 150, 1.1, 5));
-        chasers.push(new Chaser('#f8961e', target, w, h, 170, 1.08, 5));
-        chasers.push(new Chaser('#f9c74f', target, w, h, 140, 1.12, 5));
-
-        const targetInterval = 500;
-        const chaserInterval = 20;
-
-        // update targets on a relatively slow interval
-        window.setInterval(() => {
-            for (var target of targets) {
-                target.step();
-            }
-        }, targetInterval);
-
-        // update our chasers on every animation frame
-        window.setInterval(() => {
-            for (var chaser of chasers) {
-                chaser.step(chasers);
-            }
-            render(ctx, w, h, targets, chasers);
-        }, chaserInterval);
-
-        // (function loop() {
-        //     window.requestAnimationFrame(function frame() {
-        //         for (var chaser of chasers) {
-        //             chaser.step(chasers);
-        //         }
-        //         render(ctx, w, h, targets, chasers);
-        //         loop();
-        //     });
-        // })();
-    }
+    return RunSketch;
 })();
